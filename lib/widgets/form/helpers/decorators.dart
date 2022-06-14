d97c0dedef85e4b5916a5a0547508db63d8a91db
ITCRLPT491\Kamalakar <kamalakar.goretta@criticalriver.com>
2022-06-14 17:28:55 +0530

    ui fixes for dropdown textfield.

--- lib/widgets/form/helpers/decorators.dart
+++ lib/widgets/form/helpers/decorators.dart
@@ -3,24 +3,27 @@ import 'package:flutter/material.dart';
 InputDecoration getInputDecoration({double? height, String? hintText}) {
   return InputDecoration(
     focusedBorder: const OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+      borderSide:
+          BorderSide(color: Color.fromRGBO(180, 205, 251, 1), width: 2.0),
     ),
     enabledBorder: const OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+      borderSide:
+          BorderSide(color: Color.fromRGBO(180, 205, 251, 1), width: 2.0),
     ),
     errorBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: Colors.red, width: 2.0),
     ),
     disabledBorder: const OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+      borderSide:
+          BorderSide(color: Color.fromRGBO(180, 205, 251, 1), width: 2.0),
     ),
     focusedErrorBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: Colors.red, width: 2.0),
     ),
     hintText: hintText ?? '',
     contentPadding: height != null
-        ? EdgeInsets.symmetric(vertical: height, horizontal: 10)
-        : null,
+        ? EdgeInsets.fromLTRB(24, height, 10, height)
+        : const EdgeInsets.fromLTRB(24, 0, 0, 0),
   );
 }
 
